57777ed0b1dff8c2fb55609df55fd9e4a59d5ad6
Aleksandra <aleksandrainca@gmail.com>
2025-08-25 14:16:57 -0400

    API refactoring

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,32 +1,29 @@
 import { HTTP_METHOD } from '../constants';
 
-const fetchServer = (method, { id, ...payload } = {}) => {
-	let url = `http://localhost:4000/todos`;
+const fetchServer = (method, endpoint, payload) => {
+	let url = `http://localhost:4000/todos${endpoint}`;
 	let options = {
 		method,
 		headers: { 'Content-Type': 'application/json' },
 	};
 
-	if (method === HTTP_METHOD.GET) {
-		const { searchPhrase, isAlphabetSorting } = payload;
-		const sortingParams = isAlphabetSorting
-			? '_sort=title&_order=asc'
-			: '_sort=id&_order=asc';
-		url += `?${sortingParams}&title_like=${searchPhrase}`;
-	} else {
-		if (method !== HTTP_METHOD.POST) {
-			url += `/${id}`;
-		}
-		if (method !== HTTP_METHOD.DELETE) {
-			options.body = JSON.stringify(payload);
-		}
+	if (payload) {
+		options.body = JSON.stringify(payload);
 	}
 
 	return fetch(url, options).then((jsonData) => jsonData.json());
 };
 
-export const createTodo = (newTodo) => fetchServer('POST', newTodo);
-export const readTodos = (searchPhrase = '', isAlphabetSorting = false) =>
-	fetchServer('GET', { searchPhrase, isAlphabetSorting });
-export const updateTodo = (todoData) => fetchServer('PATCH', todoData);
-export const deleteTodo = (todoId) => fetchServer('DELETE', { id: todoId });
+export const createTodo = (newTodo) => fetchServer('POST', '', newTodo);
+export const readTodos = (searchPhrase = '', isAlphabetSorting = false) => {
+	let url = '?';
+	if (searchPhrase) {
+		url += `&title_like=${searchPhrase}`;
+	}
+	if (isAlphabetSorting) {
+		url += `&_sort=title&_order=asc`;
+	}
+	return fetchServer('GET', url);
+};
+export const updateTodo = (id, todoData) => fetchServer('PATCH', `/${id}`, todoData);
+export const deleteTodo = (id) => fetchServer('DELETE', `/${id}`);
